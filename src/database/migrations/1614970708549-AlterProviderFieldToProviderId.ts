f2329bd6f268c49322da0a7fa1731f94c331afb7
larissagiaccon <larissa_souz@hotmail.com>
2021-03-08 16:05:45 -0300

    Database Upload

---
+++ src/database/migrations/1614970708549-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+
+export default class AlterProviderFieldToProviderId1614970708549 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('appointments', 'provider');
+      await queryRunner.addColumn(
+        'appointments',
+        new TableColumn({
+          name: 'provider_id',
+          type: 'uuid',
+          isNullable: true,
+        })
+      );
+
+      await queryRunner.createForeignKey(
+        'appointments',
+        new TableForeignKey({
+          name: 'AppointmentProvider',
+          columnNames: ['provider_id'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'users',
+          onDelete: 'SET NULL', // RESTRICT, SET NULL, CASCATE
+          onUpdate: 'CASCADE'
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+      await queryRunner.dropColumn('appointments', 'provider_id');
+      await queryRunner.addColumn(
+        'appointments',
+        new TableColumn({
+          name: 'provider',
+          type: 'varchar',
+        })
+     );
+    }
+
+}
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+
+  // Com a atualização do TypeScript, isso se faz necessário
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return response.json({ userWithoutPassword, token });
+
+})
+
+export default sessionsRouter;
