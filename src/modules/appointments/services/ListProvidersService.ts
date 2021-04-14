635181e99c1d006c9676a0ff9591373f3dfed650
larissagiaccon <larissa_souz@hotmail.com>
2021-04-13 21:01:26 -0300

    Lista de providers com serialização

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import User from '@entitiesUsers/User';
 import IUsersRepository from '@interfaceRepositoriesUsers/IUsersRepository';
@@ -28,7 +29,10 @@ export default class ListProviderService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
